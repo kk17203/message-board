db92b1a3c6abf957fe825601e11aaf9d4172ff90
kk17203 <kk17203@gmail.com>
2023-12-09 06:21:39 -0600

    add 'new.js' and 'form.ejs'. add logic to post new message and render it.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Kevin",
+        added: new Date(),
+    },
+    {
+        text: "Hello World",
+        user: "James",
+        added: new Date(),
+    },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+/* POST a new message to the messages array from the form.ejs, then redirect to home page */
+router.post("/new", function (req, res, next) {
+    const newMessage = {
+        text: req.body.message, // message is the name associated with the input field in form.ejs
+        user: req.body.name, // name is the name associated with the input field in form.ejs
+        added: new Date(),
+    };
+    messages.push(newMessage);
+    res.redirect("/");
 });
 
 module.exports = router;
